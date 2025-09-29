862eb106c65461ae206e5eb95663c79c768ba56f
Lia-MC <lia.mc73700@gmail.com>
2025-09-29 15:32:54 -0400

    part 1 drafted for lab 3

---
+++ labs/lab3/part1.sv
@@ -0,0 +1,14 @@
+module FA (input logic a, b, cin, output logic s, cout);
+    assign s = a^b^cin;
+    assign cout = (a&b)|(cin&a)|(cin&b);
+endmodule
+
+module adder4 (input logic[3:0] A, B, input logic cin, output logic[3:0] s, output logic cout);
+
+    logic c1, c2, c3
+    FA u0(A[0], B[0], cin, s[0], c1);
+    FA u0(A[1], B[1], c1, s[1], c2);
+    FA u0(A[2], B[2], c2, s[2], c3);
+    FA u0(A[3], B[3], c3, s[3], cout);
+
+endmodule
