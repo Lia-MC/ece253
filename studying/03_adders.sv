81023852685a26cd5bf87632a9d30927e8dfd3fc
Lia-MC <lia.mc73700@gmail.com>
2025-10-05 12:31:58 -0400

    finished ripple carry adder code

--- studying/03_adders.sv
+++ studying/03_adders.sv
@@ -13,9 +13,13 @@ module full_adder(input logic a, b, carry_in, output logic[1:0] s);
     s[1] = (a & b) | (a & carry_in) | (b & carry_in);
 endmodule
 
-module ripple_carry_adder(input logic[3:0] a, b, cin, output logic[4:0] s);
+module ripple_carry_adder(input logic[3:0] a, b, cin, output logic[4:0] s, output logic cout);
     always_comb
     begin
-        full_adder u0()
+        logic c1, c2, c3;
+        full_adder u0(a[0], b[0], cin, s[0], c1);
+        full_adder u1(a[1], b[1], c1, s[1], c2);
+        full_adder u2(a[2], b[2], c2, s[2], c3);
+        full_adder u3(a[3], b[3], c3, s[3], cout);
     end
 endmodule
